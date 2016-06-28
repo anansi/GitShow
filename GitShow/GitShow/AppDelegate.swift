022e98efcbc6074089f3a9339a0b1997d84bb8b6
Julian Hulme <julian@arcadesoftware.io>
2016-06-28 14:24:55 +0200

    changes from another developer on the master branch

--- GitShow/GitShow/AppDelegate.swift
+++ GitShow/GitShow/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //new work added to the app delegate file
+        
         return true
     }
 
