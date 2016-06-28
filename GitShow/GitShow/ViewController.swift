1605f23cf0876d9aab27087ed5334ac93934caa1
Julian Hulme <julian@arcadesoftware.io>
2016-06-28 13:59:04 +0200

    added initial source code

---
+++ GitShow/GitShow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitShow
+//
+//  Created by Julian Hulme on 2016/06/28.
+//  Copyright © 2016 Julian Hulme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //new line
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
